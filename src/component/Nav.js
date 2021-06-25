86903c9c5e2ed0aecf52a86616aefbf4be42575c
HungFai Koo <jhk888@yahoo.com>
2021-06-25 15:55:14 -0700

    first commit

---
+++ src/component/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Nav(){
+    return(
+        <div>
+           <ul>
+               <li>Home</li>
+               <li>About</li>
+               <li>Project</li>
+               <li>Contact</li>
+           </ul>
+        </div>
+    )
+}
+
+export default Nav;
